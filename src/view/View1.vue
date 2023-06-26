4e89480cdcfcd525d16f07668f6aed4536af8bf8
mao <1296193245@qq.com>
2023-06-27 00:15:39 +0800

    vue组合式API-ref

---
+++ src/view/View1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>a:{{ a }}</h1>
+    <h1>b:{{ b }}</h1>
+  </div>
+</template>
+
+<script setup>
+
+import {ref} from 'vue'
+
+const a = ref(120);
+console.log(a.value)
+const b = ref("hello");
+console.log(b)
+console.log(b.value)
+
+</script>
+
+<style scoped>
+
+</style>
